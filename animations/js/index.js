9a9b0e646a4022b23e08a8118a360c4a6a7c4ac1
Jonathan Boswell <jonathan@arborbridge.com>
2016-11-01 20:01:31 -0700

    Animation Folder

---
+++ animations/js/index.js
@@ -0,0 +1,22 @@
+$('section').hide();
+$('#home').show();
+
+$(function() {
+    $('#homeLink').click(function(){
+        $('section').hide();
+        $('#home').show();
+    });
+    
+    $('li').click(function(){
+        //get the name from the <li> tag
+        var name = $(this).attr('name');
+
+        //make the clicked <li> "active"
+        $('li').removeClass();
+        $(this).addClass('active');
+
+        //show the corresponding <section>
+        $('section').hide();
+        $('#' + name).show();
+    });
+});
