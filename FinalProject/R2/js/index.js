1f45bb3fec127d9e5504aa1c76a4928cf0caeb73
Shahane <shahane@Shahanes-MBP.attlocal.net>
2016-11-06 01:11:18 -0800

    R3
    Ready for Video

---
+++ FinalProject/R2/js/index.js
@@ -0,0 +1,62 @@
+$('section').hide();
+$('#home').show();
+
+$(function() {
+    $('#homeLink').click(function(){
+        $('section').hide();
+        $('#home').show();
+    });
+    
+    $('li').click(function(){
+        //get the name from the <li> tag
+        var name = $(this).attr('name');
+
+        //make the clicked <li> "active"
+        $('li').removeClass();
+        $(this).addClass('active');
+
+        //show the corresponding <section>
+        $('section').hide();
+        $('#' + name).show();
+    });
+});
+
+
+//form 
+
+$(function(){
+    $('#emailForm').submit(function(submitEvent){
+        submitEvent.preventDefault();
+    })
+    
+    $('#send').click(function(clickEvent){
+        var email = $('#email').val();
+        var subject = $('#subject').val();
+        var message = $('#message').val();
+
+        $('#strEmail').html(email);
+        $('#strSubject').html(subject);
+        $('#strMessage').html(message);
+    });
+});
+
+
+
+
+
+// video fram home page 
+iframe = $('#projectplayer')[0]; //if you give your player a different ID, change it here
+player = $f(iframe);
+player.api('setVolume',1); //this will unmute the video
+
+
+$(document).ready(function () { 
+    
+    $('iframe[src*="vimeo.com"]').wrap('<div class="vimeoWrapper" />');
+   
+});
+
+
+
+
+
